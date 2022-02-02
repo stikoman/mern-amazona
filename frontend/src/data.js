c00020e1c1aca0fac23c1c1217ad51ef139d605e
Steven Kwasiba <stikowebdevelopment@gmail.com>
2022-02-02 12:02:00 -0300

    Video-06-Add-Page-Routing

---
+++ frontend/src/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg', // 679px × 829px
+      price: 120,
+      countInstock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Slim shirt',
+      slug: 'adidas-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInstock: 10,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Nike Slim pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 25,
+      countInstock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Slim pant',
+      slug: 'adidas-slim-pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInstock: 10,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+  ],
+};
+export default data;
